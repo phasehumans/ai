001488afc0e69f53ff578eaf95cc0b6981c7fb8d
chaitanya_s <dev.chaitanyasonawane@gmail.com>
2025-02-27 13:56:24 +0530

    conditionals and loops

---
+++ 05_JavaScript/01_conditionals_and_loops/intro.js
@@ -0,0 +1,18 @@
+console.log("chaitanya")
+
+// function ka setup
+function function_name(kitne){
+   
+    // function ka kaam
+    console.log("Chaitanya Sonawane")
+    const a= 10
+    const b= 20
+    console.log(a+b)
+    console.log(`Bring ${kitne} chai`);
+
+};
+
+// function ko call karna
+function_name(3);
+function_name(40);
+// function_name(); undefined chai
