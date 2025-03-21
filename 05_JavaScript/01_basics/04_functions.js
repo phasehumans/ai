2a8d3c89e495f26c8bc22a8b49a16ea76d15c303
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-03-21 23:37:06 +0530

    js functions

--- 05_JavaScript/01_basics/04_functions.js
+++ 05_JavaScript/01_basics/04_functions.js
@@ -1,1 +1,50 @@
 // fn : reusable block of code
+
+function fn_name (fname) {          // parameter- fname (placeholder)
+    return console.log(`${fname} sonawane`)
+}
+
+fn_name("chaitanya")        //value pass in fn is argument
+
+
+// scope: 
+let clr= "gold"
+
+function printclr(){
+    let inside_clr= clr
+    return console.log("fn indside_clr--> ",inside_clr)
+}
+
+printclr()
+console.log(clr)        //global var
+// console.log(inside_clr)         //err- ReferenceError: inside_clr is not defined ; local var
+
+
+
+// callback fn:
+function callbckfn (value, callback){
+
+    value= value*2
+    console.log(`value is ${value}`)
+    callback()
+}
+
+const truefn = () => {     // arrow fn
+    console.log(`True`)
+}
+
+callbckfn(67, truefn) // value and truefn is fn(arrow fn)
+
+
+
+// arrow fn: 
+const arrfn = ()=> {
+    console.log(`this arrow fn`)
+}
+
+const arrfn1 = (x) => {
+    console.log(`this is arrow fn with ${x}`)
+}
+
+arrfn()
+arrfn1("parameter")
