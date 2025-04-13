4202fd5fafa2679e92f5ccc1990d48fdf3e782af
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-04-13 22:42:05 +0530

    python updated

--- 02_Python/Python/01_basics/hello.py
+++
@@ -1,8 +0,0 @@
-print("hello chaitanya")
-
-# fn declaration
-def name(n):
-    print(n)
-
-# fn calling
-name("Chetan")
--- 02_Python/Python/01_basics/inwork.py
+++
@@ -1,4 +0,0 @@
-from hello import name
-name("Sonawane")
-
-# here fun in imported from other file; __pycache__ file is created
