931bc91f3c17f8356fb73b4cea4209997a393fb1
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-05-08 13:27:07 +0530

    javascript: conditionals

--- 05_JavaScript/02_controlflow/01_conditionals.js
+++ 05_JavaScript/02_controlflow/01_conditionals.js
@@ -1,4 +1,6 @@
 // use to determine the flow of program
+
+// if / else / elseif
 age = 23;
 
 if(age>18){
@@ -19,12 +21,30 @@ if (0){     //falsy
     console.log("this statment will never execute")
 }
 
+// nullish coalescing ooperator (??): null undefined (fallback)
+// do safety check if null or undeinfed then null ?? 18 ; consider 18
+let val1
+val1= 5 ?? 10
+console.log(val1)   //5
+
+let val2
+val2= null ?? 10
+console.log(val2)   //10
+
+let val3
+val3= undefined ?? 15
+console.log(val3);
+
+
+// ternary operator
+const teaPrice= 78
+teaPrice >= 80 ? console.log("less than 80"): console.log("more than 80");
+
 
 // switch 
 a= 10;
 b= 15;
 op= "*"
-
 switch(op){
 
     case "+": {
