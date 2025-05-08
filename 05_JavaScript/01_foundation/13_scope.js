e0433c8905fde46b0aaba17871b4c481749c0f8a
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-05-08 08:55:47 +0530

    js: ES6+

--- 05_JavaScript/01_foundation/13_scope.js
+++ 05_JavaScript/01_foundation/13_scope.js
@@ -31,3 +31,36 @@ example()
 // console.log(localvar2);
 
 
+
+function one(){
+    const username= "chaitanya"
+
+    // for TWO , ONE is global scope
+    function two(){
+        const website= "youtube"
+
+        console.log(username);
+        
+    }
+    // console.log(website);        //website is defined in local scope
+    two()
+}
+
+one()
+// two()       //ReferenceError: two is not defined --> defined in local scope
+
+
+//----------------  hoisting : execution context    ------------
+
+function addOne(num){
+    return num + 1
+}
+console.log(addOne(3));
+
+
+// console.log(addTwo)  ---> ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo)     //[Function: addTwo]
+console.log(addTwo(3));
