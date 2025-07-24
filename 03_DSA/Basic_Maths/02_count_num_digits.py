26fdb0a75502183f3fc1979a8739054a85b6cff5
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-07-25 00:18:46 +0530

    extraction of digits

---
+++ 03_DSA/Basic_Maths/02_count_num_digits.py
@@ -0,0 +1,10 @@
+n= 38374
+
+num= n
+res= 0
+while num > 0:
+    num % 10
+    res+=1
+    num= num // 10
+
+print(res)
---
+++ 03_DSA/Basic_Maths/03_count_num_digits_log10.py
@@ -0,0 +1,7 @@
+from math import log10
+
+n= 23554
+
+num= n
+res= int(log10(num) + 1)
+print(res)
